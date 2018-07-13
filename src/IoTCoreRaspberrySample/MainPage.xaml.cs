d6dc7f2226d4489d2db67804d924885d10937b61
Marco Diniz Garcia Gomes <msn@marcodiniz.com>
2018-07-13 00:54:50 -0300

    V1

---
+++ src/IoTCoreRaspberrySample/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace IoTCoreRaspberrySample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        TwitterManager _twitter = new TwitterManager();
+        private ThreadPoolTimer _timerTwitter;
+        private CoreDispatcher _dispacher;
+        private GpioController _gpioController;
+        private GpioPin _pinSensor;
+        private GpioPin _pinLed;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            _dispacher = this.Dispatcher;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await InitTwitter();
+            await InitGpio();
+        }
+
+        private async Task InitGpio()
+        {
+            _gpioController = await GpioController.GetDefaultAsync();
+            if (_gpioController == null)
+                return;
+
+            _pinLed = _gpioController?.OpenPin(26);
+            _pinLed.SetDriveMode(GpioPinDriveMode.Output);
+
+            _pinSensor = _gpioController?.OpenPin(18);
+            _pinSensor.DebounceTimeout = TimeSpan.FromMilliseconds(200);
+            _pinSensor.SetDriveMode(GpioPinDriveMode.InputPullDown);
+            _pinSensor.ValueChanged += _pinSensor_ValueChanged;
+        }
+
+        private async Task InitTwitter()
+        {
+            if (await _twitter.Setup())
+            {
+                this.txbMessage1.Text = "Twitter conectado!";
+                _timerTwitter = ThreadPoolTimer.CreatePeriodicTimer(Twitter_Tick, TimeSpan.FromMilliseconds(5000));
+            }
+            else
+                this.txbMessage1.Text = "Twitter off!";
+        }
+
+        private async void _pinSensor_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            var text = args.Edge == GpioPinEdge.RisingEdge ? "Tem Gente!" : "ninguém em casa";
+            await _twitter.Publish(text);
+
+            await _dispacher.RunAsync(CoreDispatcherPriority.Normal,
+                () => this.txbMessage2.Text = text);
+        }
+
+        private async void Twitter_Tick(ThreadPoolTimer timer)
+        {
+            var lastTweet = await _twitter.GetTweet();
+            if (lastTweet.user == null)
+                return;
+
+            var onOffstr = lastTweet.on ? "ligar" : "desligar";
+            _pinLed?.Write(lastTweet.on ? GpioPinValue.High : GpioPinValue.Low);
+
+            await _dispacher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                 {
+                     this.txbMessage1.Text = $"{lastTweet.user} \nmandou {onOffstr}!";
+                     this.imgProfile.Source = new BitmapImage(new Uri(lastTweet.image)); 
+                 });
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/IoTCoreRaspberrySample/TwitterManager.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Windows.Storage;
+
+namespace IoTCoreRaspberrySample
+{
+    public class TwitterCredentials
+    {
+        public string CONSUMER_KEY { get; set; } = "";
+        public string CONSUMER_SECRET { get; set; } = "";
+        public string ACCESS_TOKEN { get; set; } = "";
+        public string ACCESS_TOKEN_SECRET { get; set; } = "";
+    }
+
+    public class TwitterManager
+    {
+        private static StorageFolder _localFolder;
+        private TwitterCredentials _credentials;
+
+        public TwitterManager()
+        {
+            _localFolder = ApplicationData.Current.LocalFolder;
+        }
+
+        public async Task<bool> Setup()
+        {
+            _credentials = await LoadJson<TwitterCredentials>("credentials.txt") ?? new TwitterCredentials();
+
+            //apenas para gerar o arquivo inicial
+            SaveJson(_credentials, "credentials.txt");
+
+            Auth.SetUserCredentials(_credentials.CONSUMER_KEY, _credentials.CONSUMER_SECRET, _credentials.ACCESS_TOKEN, _credentials.ACCESS_TOKEN_SECRET);
+            var user = User.GetAuthenticatedUser();
+            if (user == null || string.IsNullOrWhiteSpace(user.Name))
+                return false;
+
+            return true;
+        }
+
+        public async Task Publish(string text)
+        {
+            var result = Tweet.PublishTweet(text + " #W10IoTCore #TheDevConf");
+        }
+
+        public async Task<(bool on, string user, string image)> GetTweet()
+        {
+            var matchingTweets = await Task.Run(() => Search.SearchTweets("#TdcIot"));
+            var tweet = matchingTweets.FirstOrDefault();
+            if (tweet == null)
+                return (false, null, null);
+
+            var on = tweet.FullText.ToLowerInvariant().Contains("on");
+            var name = $"{tweet.CreatedBy.Name}\n@{tweet.CreatedBy.ScreenName}";
+            return (on, name, tweet.CreatedBy.ProfileImageUrl400x400);
+        }
+
+        public static async Task<T> LoadJson<T>(string file)
+        {
+            var f = await _localFolder.TryGetItemAsync(file);
+            if (f == null)
+                await _localFolder.CreateFileAsync(file);
+
+            var readStream = await _localFolder.OpenStreamForReadAsync(file);
+            var reader = new StreamReader(readStream);
+            var str = reader.ReadToEnd();
+            T result = JsonConvert.DeserializeObject<T>(str);
+            return result;
+        }
+
+        public static async Task SaveJson<T>(T obj, string file) where T : class
+        {
+            var f = await _localFolder.TryGetItemAsync(file);
+            if (f == null)
+                await _localFolder.CreateFileAsync(file);
+
+            var writeStream = await _localFolder.OpenStreamForWriteAsync(file, CreationCollisionOption.ReplaceExisting);
+            var writer = new StreamWriter(writeStream);
+            var str = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            await writer.WriteAsync(str);
+            await writer.FlushAsync();
+            writeStream.Dispose();
+        }
+    }
+}
